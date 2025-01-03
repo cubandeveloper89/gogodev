4e469dc00a349efa20c0f0625f2124016daa0429
Camilo Garcia <cr7habana@gmail.com>
2025-01-03 10:44:02 -0500

    added animated placeholder

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <div>Mi VUE app!!! with Vite</div>
-  <OrderDetails/>
+  <div>Loading Effect</div>
+  <OrderDetailLoader v-if="loading"/>
+  <OrderDetails v-else/>
   
 </template>
 
 <script lang="ts" setup>
 
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import OrderDetails from './components/OrderDetails.vue';
+import OrderDetailLoader from './components/orderDetailLoader.vue';
 
 
 
@@ -24,9 +26,15 @@ import OrderDetails from './components/OrderDetails.vue';
   // data(){
     let nombre = 'Camilo';
     let count = ref(0);
+    let loading = ref(true);
     // return {numero}
   // }
 // })
+onMounted(() => {
+  setTimeout(() => {
+    loading.value = false;
+  }, 1000);
+});
 </script>
 
 <style>
---
+++ src/components/orderDetailLoader.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="flex flex-col gap-6 p-4">
+      <div class="flex justify-between items-center border-b pb-2">
+        <div class="relative w-full flex flex-row gap-1 lg:gap-2 justify-between">
+          <p
+            class="font-roboto font-bold text-base leading-[16.8px] lg:leading-[19.2px] text-neutral-1000"
+          >
+            <AnimatedPlaceHolder height="25px" width="80px"/>
+          </p>
+          <p
+            class="font-roboto font-normal text-sm lg:text-md leading-[14.4px] lg:leading-[21px] text-neutral-900"
+          >
+          <AnimatedPlaceHolder height="25px" width="80px"/>
+          </p>
+        </div>
+      </div>
+      <div class="w-full flex sm:flex-row xs:flex-col justify-between gap-4">
+        <div class="w-full bg-white flex flex-col gap-6 p-4 border border-[#CCCCCC] rounded">
+            <AnimatedPlaceHolder height="45px" width="320px"/>
+        </div>
+        <div class="w-full bg-white flex flex-col gap-6 p-4 border border-[#CCCCCC] rounded">
+            <AnimatedPlaceHolder height="45px" width="320px"/>
+        </div>
+      </div>
+  
+      <div class="w-full flex flex-col gap-4">
+        <div class="flex justify-between items-center">
+          <p class="font-roboto-condensed font-bold text-xl leading-6"><AnimatedPlaceHolder height="25px" width="80px"/></p>
+        </div>
+        <div class="w-full bg-white flex flex-col gap-6 p-4 border border-[#CCCCCC] rounded"><AnimatedPlaceHolder height="45px" width="320px"/></div>
+        
+      </div>
+      <div><AnimatedPlaceHolder height="25px" width="120px"/></div>
+      
+    </div>
+</template>
+
+<script lang="ts" setup>
+import AnimatedPlaceHolder from './AnimatedPlaceHolder.vue';
+
+</script>
