da33e8a13e05d3d7a9f53856c82ee8e3eb34689a
Camilo Garcia <cr7habana@gmail.com>
2025-01-04 12:17:31 -0500

    adding the font-size directive

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,15 @@ import './assets/css/tailwind.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.directive('font-size', {
+    beforeMount: (el, binding) =>{
+        el.style.fontSize = binding.value + 'px';
+        el.style.color = 'red';
+    },
+})
+
+
+
+app.mount('#app')
